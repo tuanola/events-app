ffac8e0d8a9aed021b4bedb619bde9cfdd7a03eb
Anna Shtanova <tuanola@gmail.com>
2017-11-14 18:48:33 +0400

    Connect Search field and Tags by Vuex

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -2,8 +2,8 @@
   <div class="searchbox">
     <form id="search" @submit.prevent="emitSearchEvent()">
       <div class="row">
-        <div class="col-md-7"><input name="query" v-model="searchQuery" @keyup="updateFilter()" class="form-control" placeholder="Event or person you are looking for"></div>
-        <div class="col-md-3"><input name="location" v-model="searchLocation" @keyup="updateFilter()" class="form-control" placeholder="Where"></div>
+        <div class="col-md-7"><input name="query" v-model="query" class="form-control" placeholder="Event or person you are looking for"></div>
+        <div class="col-md-3"><input name="location" v-model="location" class="form-control" placeholder="Where"></div>
         <div class="col-md-2"><a href="#" class="btn btn-primary" @click="updateFilter()">Search</a></div>
       </div>
     </form>
@@ -17,15 +17,30 @@
   import { SET_FILTER_CONDITIONS } from '../store/mutation-types'
 
   export default {
-    data: function () {
-      return {
-        searchQuery: '',
-        searchLocation: ''
+    computed: {
+      query: {
+        get () {
+          return this.$store.state.filterConditions.what
+        },
+        set (value) {
+          this.commitFilter(value, this.location)
+        }
+      },
+      location: {
+        get () {
+          return this.$store.state.filterConditions.where
+        },
+        set (value) {
+          this.commitFilter(this.query, value)
+        }
       }
     },
     methods: {
       updateFilter () {
-        this.$store.commit(SET_FILTER_CONDITIONS, { what: this.searchQuery, where: this.searchLocation })
+        this.commitFilter(this.query, this.location)
+      },
+      commitFilter (what, where) {
+        this.$store.commit(SET_FILTER_CONDITIONS, { what, where })
       }
     }
   }
